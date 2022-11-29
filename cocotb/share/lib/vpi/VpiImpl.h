b75f4736c4aad219fc5a4a88600a18acf25c465f
Vladislav Rumiantsev <30479522+vladrumyan@users.noreply.github.com>
2022-11-29 10:50:18 +0000

    Add missing override for sim_stop()

--- cocotb/share/lib/vpi/VpiImpl.h
+++ cocotb/share/lib/vpi/VpiImpl.h
@@ -262,6 +262,7 @@ class VpiImpl : public GpiImplInterface {
 
     /* Sim related */
     void sim_end(void) override;
+    void sim_stop(void) override;
     void get_sim_time(uint32_t *high, uint32_t *low) override;
     void get_sim_precision(int32_t *precision) override;
     const char *get_simulator_product() override;
